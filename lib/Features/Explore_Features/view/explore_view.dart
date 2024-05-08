1f849c7b22df2f794a59a017b893867403115a56
MohsenModhej13 <mohsenmodhej98@gmail.com>
2024-05-08 15:16:22 +0330

    explore added

---
+++ lib/Features/Explore_Features/view/explore_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ExploreView extends StatefulWidget {
+  const ExploreView({super.key});
+
+  @override
+  State<ExploreView> createState() => _ExploreViewState();
+}
+
+class _ExploreViewState extends State<ExploreView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
