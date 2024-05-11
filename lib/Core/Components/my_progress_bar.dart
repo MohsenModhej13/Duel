f65e0eb031169aa795ec9caabdf71542eceeab7a
pouriabbasi <99184655+pouriabbasi@users.noreply.github.com>
2024-05-11 19:50:10 +0330

    add progress bar

---
+++ lib/Core/Components/my_progress_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class MyProgressBar extends StatelessWidget {
+  MyProgressBar({super.key,this.color = Colors.blueAccent, this.size = 25});
+  final Color color;
+  final double size;
+  @override
+  Widget build(BuildContext context) {
+    return SpinKitCubeGrid(
+      color: color,
+      size: size,
+    );
+  }
+}
